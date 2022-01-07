28acdbfc8b7e0abe0a071f74d5f238bf99b4462f
장정원 <longthegarden@innodep.com>
2022-01-07 13:34:45 +0900

    함수 및 클래스 Component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function tick() {
-    const element = (
-        <div>
-            <h1>Hello, world!</h1>
-            <h2>It is {new Date().toLocaleTimeString()}.</h2>
-        </div>
-    );
-    ReactDOM.render(element, document.getElementById('root'));
+// function Welcome(props) {
+//     return <h1>Hello, {props.name}</h1>;
+// }
+
+class Welcome extends React.Component {
+    render() {
+        return <h1>Hello, {this.props.name}</h1>;
+    }
 }
 
-setInterval(tick, 1000);
+ReactDOM.render(<Welcome name="test"/>, document.getElementById('root'));
