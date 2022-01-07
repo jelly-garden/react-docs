897300c5d53111c8c86c56de460f2fc77dea33b0
장정원 <longthegarden@innodep.com>
2022-01-07 14:25:41 +0900

    7. 조건부 렌더링  - Element 변수

--- src/index.js
+++ src/index.js
@@ -1,6 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class LoginControl extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleLoginClick = this.handleLoginClick.bind(this);
+        this.handleLogoutClick = this.handleLogoutClick.bind(this);
+        this.state = {isLoggedIn: false};
+    }
+
+    handleLoginClick() {
+        this.setState({isLoggedIn: true});
+    }
+
+    handleLogoutClick() {
+        this.setState({isLoggedIn: false});
+    }
+
+    render() {
+        const isLoggedIn = this.state.isLoggedIn;
+        let button;
+
+        if (isLoggedIn) {
+            button = <LogoutButton onClick={this.handleLogoutClick} />;
+        } else {
+            button = <LoginButton onClick={this.handleLoginClick} />;
+        }
+
+        return (
+            <div>
+                <Greeting isLoggedIn={isLoggedIn} />
+                {button}
+            </div>
+        );
+    }
+}
+
 function UserGreeting(props) {
     return <h1>Welcome back!</h1>;
 }
@@ -17,8 +52,23 @@ function Greeting(props) {
     return <GuestGreeting />;
 }
 
+function LoginButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Login
+        </button>
+    );
+}
+
+function LogoutButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Logout
+        </button>
+    );
+}
+
 ReactDOM.render(
-    // Try changing to isLoggedIn={true}:
-    <Greeting isLoggedIn={false} />,
+    <LoginControl />,
     document.getElementById('root')
 );
