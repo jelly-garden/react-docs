0e689d67fc9893257bd477b51b1de6a25167f28d
장정원 <longthegarden@innodep.com>
2022-01-07 14:30:40 +0900

    7. 조건부 렌더링  - Component 렌더링 방지

--- src/index.js
+++ src/index.js
@@ -1,69 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class LoginControl extends React.Component {
+function WarningBanner(props) {
+    if (!props.warn) {
+        return null;
+    }
+
+    return (
+        <div className="warning">
+            Warning!
+        </div>
+    );
+}
+
+class Page extends React.Component {
     constructor(props) {
         super(props);
-        this.handleLoginClick = this.handleLoginClick.bind(this);
-        this.handleLogoutClick = this.handleLogoutClick.bind(this);
-        this.state = {isLoggedIn: false};
+        this.state = {showWarning: true}
+        this.handleToggleClick = this.handleToggleClick.bind(this);
     }
 
-    handleLoginClick() {
-        this.setState({isLoggedIn: true});
-    }
-
-    handleLogoutClick() {
-        this.setState({isLoggedIn: false});
+    handleToggleClick() {
+        this.setState(prevState => ({
+            showWarning: !prevState.showWarning
+        }));
     }
 
     render() {
-        const isLoggedIn = this.state.isLoggedIn;
         return (
             <div>
-                <Greeting isLoggedIn={isLoggedIn} />
-                {isLoggedIn
-                    ? <LogoutButton onClick={this.handleLogoutClick} />
-                    : <LoginButton onClick={this.handleLoginClick} />
-                }
+                <WarningBanner warn={this.state.showWarning} />
+                <button onClick={this.handleToggleClick}>
+                    {this.state.showWarning ? 'Hide' : 'Show'}
+                </button>
             </div>
         );
     }
 }
 
-function UserGreeting(props) {
-    return <h1>Welcome back!</h1>;
-}
-
-function GuestGreeting(props) {
-    return <h1>Please sign up.</h1>;
-}
-
-function Greeting(props) {
-    const isLoggedIn = props.isLoggedIn;
-    if (isLoggedIn) {
-        return <UserGreeting />;
-    }
-    return <GuestGreeting />;
-}
-
-function LoginButton(props) {
-    return (
-        <button onClick={props.onClick}>
-            Login
-        </button>
-    );
-}
-
-function LogoutButton(props) {
-    return (
-        <button onClick={props.onClick}>
-            Logout
-        </button>
-    );
-}
-
 ReactDOM.render(
-    <LoginControl />,
+    <Page />,
     document.getElementById('root')
 );
