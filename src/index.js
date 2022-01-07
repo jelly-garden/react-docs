aec1a82dedc232907816d230537a06cf8c33d2a7
장정원 <longthegarden@innodep.com>
2022-01-07 13:36:02 +0900

    Component 렌더링

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// function Welcome(props) {
-//     return <h1>Hello, {props.name}</h1>;
-// }
-
-class Welcome extends React.Component {
-    render() {
-        return <h1>Hello, {this.props.name}</h1>;
-    }
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>;
 }
 
-ReactDOM.render(<Welcome name="test"/>, document.getElementById('root'));
+const element = <Welcome name="Sara" />;
+
+ReactDOM.render(
+    element,
+    document.getElementById('root')
+);
