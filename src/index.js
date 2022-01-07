32c281ade4ddbea95f355bd9d34666321c9696a0
장정원 <longthegarden@innodep.com>
2022-01-07 14:42:54 +0900

    8. 목록 및 키  - 키로 Component 추출

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function ListItem(props) {
+    // Correct! There is no need to specify the key here:
+    return <li>{props.value}</li>;
+}
+
 function NumberList(props) {
     const numbers = props.numbers;
     const listItems = numbers.map((number) =>
-        <li key={number.toString()}>
-            {number}
-        </li>
+        // Correct! Key should be specified inside the array.
+        <ListItem key={number.toString()}
+                  value={number} />
     );
     return (
-        <ul>{listItems}</ul>
+        <ul>
+            {listItems}
+        </ul>
     );
 }
 
