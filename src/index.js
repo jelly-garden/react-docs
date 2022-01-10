7af47a08b9c96f0c03a2c208deaf6047579ed454
장정원 <longthegarden@innodep.com>
2022-01-10 13:54:19 +0900

    10. 상태 올리기  - 온도 계산기 생성

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Reservation from './docs/reservation';
+import Calculator from './docs/calculator';
 
 ReactDOM.render(
-    <Reservation />,
+    <Calculator />,
     document.getElementById('root')
 );
