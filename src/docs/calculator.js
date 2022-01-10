0e34946572d80e0d4f6cf22766f5ed5911bbada8
장정원 <longthegarden@innodep.com>
2022-01-10 14:00:23 +0900

    10. 상태 올리기  - 온도계산기 입력 추가 : celsius, fahrenheit

--- src/docs/calculator.js
+++ src/docs/calculator.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-function BoilingVerdict(props) {
-    if (props.celsius >= 100) {
-        return <p>The water would boil.</p>;
-    }
-    return <p>The water would not boil.</p>;
-}
+const scaleNames = {
+    c: 'Celsius',
+    f: 'Fahrenheit'
+};
 
-class Calculator extends React.Component {
+class TemperatureInput extends React.Component {
     constructor(props) {
         super(props);
         this.state = {temperature: ''};
@@ -21,15 +19,24 @@ class Calculator extends React.Component {
 
     render() {
         const temperature = this.state.temperature;
+        const scale = this.props.scale;
         return (
             <fieldset>
-                <legend>Enter temperature in Celsius:</legend>
-                <input
-                    value={temperature}
-                    onChange={this.handleChange} />
-                <BoilingVerdict
-                    celsius={parseFloat(temperature)} />
+                <legend>Enter temperature in {scaleNames[scale]}:</legend>
+                <input value={temperature}
+                       onChange={this.handleChange} />
             </fieldset>
+        );
+    }
+}
+
+class Calculator extends React.Component {
+    render() {
+        return (
+            <div>
+                <TemperatureInput scale="c" />
+                <TemperatureInput scale="f" />
+            </div>
         );
     }
 }
